f0d1e3450740f04b166aba5a7529075a86a562f6
Rick van Voorden <rick@northbronson.com>
2025-05-12 16:39:28 -0700

    Updated FoodTruckApp.

--- App/App.swift
+++ App/App.swift
@@ -7,6 +7,9 @@ The single entry point for the Food Truck app on iOS and macOS.
 
 import SwiftUI
 import FoodTruckKit
+#if canImport(ActivityKit)
+import ActivityKit
+#endif
 import ImmutableData
 import ImmutableUI
 
@@ -26,6 +29,11 @@ struct FoodTruckApp: App {
         reducer: FoodTruckReducer.reduce
     )
     @State private var modelListener = ModelListener()
+    #if canImport(ActivityKit)
+    @State private var storeListener = StoreListener(session: TruckActivitySession())
+    #else
+    @State private var storeListener = StoreListener()
+    #endif
     
     /// The app's body function.
     ///
@@ -42,6 +50,12 @@ struct FoodTruckApp: App {
                 self.modelListener.listen(
                     to: self.model,
                     with: self.store
+                )
+            }
+            .onAppear {
+                self.storeListener.listen(
+                    to: self.store,
+                    with: self.model
                 )
             }
         }
@@ -64,3 +78,85 @@ struct FoodTruckApp: App {
         #endif
     }
 }
+
+#if canImport(ActivityKit)
+@MainActor final class TruckActivitySession: StoreListenerActivitySession {
+  
+}
+#endif
+
+#if canImport(ActivityKit)
+extension TruckActivitySession {
+  private func postNotification() {
+    let timerSeconds = 60
+    let content = UNMutableNotificationContent()
+    content.title = "Donuts are done!"
+    content.body = "Time to take them out of the oven."
+    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(timerSeconds), repeats: false)
+    let uuidString = UUID().uuidString
+    let request = UNNotificationRequest(identifier: uuidString,
+                                        content: content, trigger: trigger)
+    let notificationCenter = UNUserNotificationCenter.current()
+    Task {
+      do {
+        try await notificationCenter.requestAuthorization(options: [.alert, .sound, .badge])
+        do {
+          try await notificationCenter.add(request)
+          print("Posted local notification.")
+        } catch {
+          print("Error posting local notification: \(error.localizedDescription)")
+        }
+      } catch {
+        print("Error requesting authorization: \(error.localizedDescription)")
+      }
+    }
+  }
+}
+#endif
+
+#if canImport(ActivityKit)
+extension TruckActivitySession {
+  func startActivity(_ order: Order) {
+    let timerSeconds = 60
+    let activityAttributes = TruckActivityAttributes(
+      orderID: String(order.id.dropFirst(6)),
+      order: order.donuts.map(\.id),
+      sales: order.sales,
+      activityName: "Order preparation activity."
+    )
+    
+    let future = Date(timeIntervalSinceNow: Double(timerSeconds))
+    
+    let initialContentState = TruckActivityAttributes.ContentState(timerRange: Date.now...future)
+    
+    let activityContent = ActivityContent(state: initialContentState, staleDate: Calendar.current.date(byAdding: .minute, value: 2, to: Date())!)
+    
+    do {
+      let myActivity = try Activity<TruckActivityAttributes>.request(
+        attributes: activityAttributes,
+        content: activityContent,
+        pushType: nil
+      )
+      print(" Requested MyActivity live activity. ID: \(myActivity.id)")
+      postNotification()
+    } catch let error {
+      print("Error requesting live activity: \(error.localizedDescription)")
+    }
+  }
+}
+#endif
+
+#if canImport(ActivityKit)
+extension TruckActivitySession {
+  func endActivity(_ order: Order) {
+    Task {
+      for activity in Activity<TruckActivityAttributes>.activities {
+        // Check if this is the activity associated with this order.
+        if activity.attributes.orderID == String(order.id.dropFirst(6)) {
+          await activity.end(nil, dismissalPolicy: .immediate)
+        }
+      }
+    }
+  }
+}
+#endif
