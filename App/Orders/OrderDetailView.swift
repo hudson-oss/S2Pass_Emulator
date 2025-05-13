cff93133262dc8e829a04f4276fa6467a0ca3506
Rick van Voorden <rick@northbronson.com>
2025-05-12 17:20:27 -0700

    Updated OrderDetailView.

--- App/Orders/OrderDetailView.swift
+++ App/Orders/OrderDetailView.swift
@@ -7,16 +7,19 @@ The order detail view.
 
 import SwiftUI
 import FoodTruckKit
-#if canImport(ActivityKit)
-import ActivityKit
-#endif
 
 struct OrderDetailView: View {
-    @Binding var order: Order
+    @SelectOrder private var order: Order?
+    
+    init(orderID: Order.ID) {
+        self._order = SelectOrder(orderID: orderID)
+    }
+    
+    @Dispatch private var dispatch
     
     @State private var presentingCompletionSheet = false
     
-    var body: some View {
+    private func content(_ order: Order) -> some View {
         List {
             Section("Status") {
                 HStack {
@@ -60,101 +63,61 @@ struct OrderDetailView: View {
         .toolbar {
             ToolbarItemGroup {
                 Button {
-                    order.markAsNextStep { status in
-                        if status == .preparing {
-                            #if canImport(ActivityKit)
-                            print("Start live activity.")
-                            prepareOrder()
-                            #endif
-                        } else if status == .completed {
-                            #if canImport(ActivityKit)
-                            print("Stop live activity.")
-                            endActivity()
-                            #endif
-                        }
-                    }
+                    onTapStatusButton(orderID: order.id)
                 } label: {
                     Label(order.status.buttonTitle, systemImage: order.status.iconSystemName)
                         .symbolVariant(order.isComplete ? .fill : .none)
                 }
                 .labelStyle(.iconOnly)
-                .disabled(order.isComplete)
+                .disabled(order.isComplete || order.status == .ready)
             }
         }
     }
     
-    #if canImport(ActivityKit)
-    func prepareOrder() {
-        let timerSeconds = 60
-        let activityAttributes = TruckActivityAttributes(
-            orderID: String(order.id.dropFirst(6)),
-            order: order.donuts.map(\.id),
-            sales: order.sales,
-            activityName: "Order preparation activity."
-        )
-        
-        let future = Date(timeIntervalSinceNow: Double(timerSeconds))
-        
-        let initialContentState = TruckActivityAttributes.ContentState(timerRange: Date.now...future)
-        
-        let activityContent = ActivityContent(state: initialContentState, staleDate: Calendar.current.date(byAdding: .minute, value: 2, to: Date())!)
-        
+    var body: some View {
+        if let order = self.order {
+            self.content(order)
+        } else {
+            Text("missing order").padding()
+        }
+    }
+}
+
+extension OrderDetailView {
+    @available(*, deprecated)
+    init(order: Order) {
+        self.init(orderID: order.id)
+    }
+}
+
+extension OrderDetailView {
+    @available(*, deprecated)
+    init(order: Binding<Order>) {
+        self.init(order: order.wrappedValue)
+    }
+}
+
+extension OrderDetailView {
+    private func onTapStatusButton(orderID: Order.ID) {
         do {
-            let myActivity = try Activity<TruckActivityAttributes>.request(attributes: activityAttributes, content: activityContent,
-                pushType: nil)
-            print(" Requested MyActivity live activity. ID: \(myActivity.id)")
-            postNotification()
-        } catch let error {
-            print("Error requesting live activity: \(error.localizedDescription)")
+            try self.dispatch(.ui(.orderDetailView(.onTapStatusButton(orderID: orderID))))
+        } catch {
+            print(error)
         }
     }
-    
-    func endActivity() {
-        Task {
-            for activity in Activity<TruckActivityAttributes>.activities {
-                // Check if this is the activity associated with this order.
-                if activity.attributes.orderID == String(order.id.dropFirst(6)) {
-                    await activity.end(nil, dismissalPolicy: .immediate)
-                }
-            }
-        }
-    }
-    #endif
-    
-    #if os(iOS)
-    func postNotification() {
-        let timerSeconds = 60
-        let content = UNMutableNotificationContent()
-        content.title = "Donuts are done!"
-        content.body = "Time to take them out of the oven."
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(timerSeconds), repeats: false)
-        let uuidString = UUID().uuidString
-        let request = UNNotificationRequest(identifier: uuidString,
-                    content: content, trigger: trigger)
-        let notificationCenter = UNUserNotificationCenter.current()
-        notificationCenter.add(request) { (error) in
-           if error != nil {
-              // Handle any errors.
-               print("Error posting local notification: \(error?.localizedDescription ?? "no description")")
-           } else {
-               print("Posted local notification.")
-           }
-        }
-    }
-    #endif
 }
 
 struct OrderDetailView_Previews: PreviewProvider {
-    struct Preview: View {
-        @State private var order = Order.preview
-        var body: some View {
-            OrderDetailView(order: $order)
-        }
-    }
-    
     static var previews: some View {
-        NavigationStack {
-            Preview()
+        PreviewStore {
+            NavigationStack {
+                OrderDetailView(orderID: Order.preview.id)
+            }
+        }
+        PreviewStore {
+            NavigationStack {
+                OrderDetailView(orderID: "")
+            }
         }
     }
 }
